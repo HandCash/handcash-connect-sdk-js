d38cd23db571f9e23f6ba23e4c25ad554287c840
eye1 <levascena@gmail.com>
2022-09-28 17:55:05 +0200

    coverage

--- test/unit/api/http_request_factory.test.js
+++ test/unit/api/http_request_factory.test.js
@@ -9,16 +9,30 @@ describe('# HttpRequestFactory - Unit Tests', () => {
    it('should raise an invalid auth token error', async () => {
       const authToken = 'invalid-token-123';
       const appSecret = '1234567890';
-      return expect(() => new HttpRequestFactory(authToken, Environments.iae.apiEndpoint, appSecret))
+      const appId = 'id1';
+      return expect(() => new HttpRequestFactory(authToken, Environments.iae.apiEndpoint,
+         Environments.iae.trustholderEndpoint, appSecret, appId))
          .to
          .throw('Invalid authToken');
    });
 
-   it('should raise an invalid app secret error', async () => {
+   it('should raise missing app secret error', async () => {
       const authToken = 'E9873D79C6D87DC0FB6A5778633389F4453213303DA61F20BD67FC233AA33262';
       const appSecret = undefined;
-      return expect(() => new HttpRequestFactory(authToken, Environments.iae.apiEndpoint, appSecret))
+      const appId = 'id1';
+      return expect(() => new HttpRequestFactory(authToken, Environments.iae.apiEndpoint,
+         Environments.iae.trustholderEndpoint, appSecret, appId))
          .to
          .throw('Missing appSecret');
+   });
+
+   it('should raise missing app id error', async () => {
+      const authToken = 'E9873D79C6D87DC0FB6A5778633389F4453213303DA61F20BD67FC233AA33262';
+      const appSecret = `1234567890`;
+      const appId = undefined;
+      return expect(() => new HttpRequestFactory(authToken, Environments.iae.apiEndpoint,
+         Environments.iae.trustholderEndpoint, appSecret, appId))
+         .to
+         .throw('Missing appId');
    });
 });
