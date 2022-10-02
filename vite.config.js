0e05c5d8bb3092b41776eca6a7abb9a88ed249da
Alexei Torgashov <alexei.torgashov@yahoo.de>
2022-10-02 22:44:31 +0200

    fix package.json for publishing to npm

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			// Could also be a dictionary or array of multiple entry points
-			entry: resolve(__dirname, 'src/index.ts'),
+			entry: 'src/index.ts',
 			name: 'HandCash Connect SDK',
 			formats: ['es'],
 			fileName: 'handcash-connect-sdk-ts',
