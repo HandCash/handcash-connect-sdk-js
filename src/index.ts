ba7ff359fb2e51e36009c8c8201612390d73b5c6
Brandon Bryant <brandon@Brandons-iMac.local>
2023-11-28 16:40:19 -0500

    export many types

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,7 @@ import HandCashOwner from './run_extension/handcash_owner';
 import HandCashConnectApiError from './api/handcash_connect_api_error';
 import Environments from './environments';
 import HandCashMinter from './handcash_minter';
-import * as ItemTypes from './types/items';
-import * as EventTypes from './types/events';
-import * as PaymentTypes from './types/payments';
-
-const Types = {
-	...ItemTypes,
-	...EventTypes,
-	...PaymentTypes,
-};
+import * as Types from './types/index';
 
 const Permissions = {
 	Pay: 'PAY',
